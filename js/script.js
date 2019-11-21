32e89a4302a9d43235c33d3064c5457f2a1f3f36
Diane de Lassence <dianedelassence@gmail.com>
2019-11-21 16:52:52 +0100

    effet de parallax

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Effet de parallaxe
+
+$(window).scroll(function (e) {
+    parallax();
+});
+function parallax() {
+
+    var scrolled = $(window).scrollTop();
+
+    $('#logo').css('top', -(scrolled * 0.4) + 'px');
+}
